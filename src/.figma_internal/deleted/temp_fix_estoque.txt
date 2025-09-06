4a0206658f2f46b711e8ad7a437a3da88afc2d49
Rômulo Sutil <sutil.romulo@gmail.com>
2025-09-06 17:45:55 -0300

    Ajustes de fail deploy

--- src/.figma_internal/deleted/temp_fix_estoque.txt
+++
@@ -1,4 +0,0 @@
-Correções necessárias nas linhas:
-- Linha 559: produto.estoqueMinimo -> produto.minimo  
-- Linha 597: produto.estoqueMinimo -> produto.minimo
-- Linha 601: produto.estoqueMinimo -> produto.minimo
--- src/components/Code-component-78-1070.tsx
+++
@@ -1,9 +0,0 @@
-# COMPONENTE REMOVIDO
-
-Este arquivo foi removido durante a refatoração estratégica.
-
-**Motivo**: Componente obsoleto - versão antiga do sistema de estoque.
-
-**Substituto**: EstoqueModerno.tsx (componente ativo)
-
-**Data**: Refatoração estratégica do sistema Meu Bentin
